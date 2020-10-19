78e1a7bc107f65b9981589f17dd37b88e12a93a4
xSt4rLordx <51536487+xSt4rLordx@users.noreply.github.com>
2020-10-18 23:51:18 -0700

    Updates

---
+++ precache-manifest.121d21aef71ce6e7f2fcd1ac422b047f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcc49046ab713344023993a3d860d7d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "93ca7466153d671ca483",
+    "url": "/portfolio/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "c9524bddf711738b0d80",
+    "url": "/portfolio/static/css/main.372d0506.chunk.css"
+  },
+  {
+    "revision": "93ca7466153d671ca483",
+    "url": "/portfolio/static/js/2.72bb087b.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/portfolio/static/js/2.72bb087b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9524bddf711738b0d80",
+    "url": "/portfolio/static/js/main.2f34808c.chunk.js"
+  },
+  {
+    "revision": "4b621fb36b3573181d80",
+    "url": "/portfolio/static/js/runtime-main.3f553171.js"
+  },
+  {
+    "revision": "4bd337368519a7f842f1865a5d031237",
+    "url": "/portfolio/static/media/ak.4bd33736.jpg"
+  },
+  {
+    "revision": "465faff2d70a72c2363cee2388910829",
+    "url": "/portfolio/static/media/fb.465faff2.png"
+  },
+  {
+    "revision": "b473fcad595f09b0afe1270d9016ac8c",
+    "url": "/portfolio/static/media/in.b473fcad.png"
+  },
+  {
+    "revision": "9e4070741781f01a17b1aece559ecbaf",
+    "url": "/portfolio/static/media/js.9e407074.png"
+  },
+  {
+    "revision": "7fb99744eaccae943d5adf537b5b3a93",
+    "url": "/portfolio/static/media/lk.7fb99744.png"
+  },
+  {
+    "revision": "0178326b24b10e794ee2b180c926e4c9",
+    "url": "/portfolio/static/media/lptp.0178326b.png"
+  },
+  {
+    "revision": "6f0126bbdedbdd873acb5617da77f423",
+    "url": "/portfolio/static/media/phone.6f0126bb.png"
+  },
+  {
+    "revision": "10e47b8b3e5449ec77026682926cef33",
+    "url": "/portfolio/static/media/react.10e47b8b.png"
+  },
+  {
+    "revision": "e6817a367a89f4df51006cdf05c04527",
+    "url": "/portfolio/static/media/wts.e6817a36.png"
+  }
+]);
